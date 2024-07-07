3fa29f1bebe4c08b99998fc55e07eb72e7c02bd7
perawitayasa <perawitayasa@gmail.com>
2024-07-07 20:22:04 +0000

    up

--- app/api/send-notification/route.js
+++ app/api/send-notification/route.js
@@ -1,12 +1,8 @@
 import { NextResponse } from "next/server";
-import Twilio from "@/lib/twilio";
 
 export async function POST(req, res) {
   try {
-      const data = await req.json();
-      const twilio = Twilio();
-      const response = await twilio.send_wa(data);
-      return NextResponse.json({ massage: response });
+    return NextResponse.json({ massage: 'success' });
   } catch (error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
   }
