1b354dab38e9c545c9a99fc35e38c63a172cf525
perawitayasa <perawitayasa@gmail.com>
2024-07-08 12:20:55 +0000

    reverse components card

--- app/views/places/components/card.tsx
+++ app/views/places/components/card.tsx
@@ -1,20 +1,28 @@
 "use client";
-
-import { useRef, useState } from "react";
-import Image from "next/image";
-import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/utils/cn";
-import { DirectionAwareHover } from "@/components/ui/direction-aware-hover";
 
 export function Card() {
-  const imageUrl =
-    "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
-    <div className="h-[40rem] relative  flex items-center justify-center">
-      <DirectionAwareHover imageUrl={imageUrl}>
-        <p className="font-bold text-xl">In the mountains</p>
-        <p className="font-normal text-sm">$1299 / night</p>
-      </DirectionAwareHover>
+    <div className="max-w-xs w-full">
+      <div
+        className={cn(
+          "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+          "bg-[url(https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80)] bg-cover",
+          // Preload hover image by setting it in a pseudo-element
+          "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+          "transition-all duration-500"
+        )}
+      >
+        <div className="text relative z-50">
+          <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+            Background Overlays
+          </h1>
+          <p className="font-normal text-base text-gray-50 relative my-4">
+            This card is for some special elements, like displaying background
+            gifs on hover only.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
