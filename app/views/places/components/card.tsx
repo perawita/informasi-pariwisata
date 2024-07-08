ff5331ea45cfd590a40694bb4d07c97714d819be
perawitayasa <perawitayasa@gmail.com>
2024-07-08 15:52:46 +0000

    fixed components card

--- app/views/places/components/card.tsx
+++ app/views/places/components/card.tsx
@@ -1,25 +1,29 @@
 "use client";
 import { cn } from "@/utils/cn";
 
-export function Card() {
+interface CardProps {
+  Title: string;
+  Description: string;
+  Background: string;
+}
+
+export function Card({ Title, Description, Background }: CardProps) {
   return (
     <div className="max-w-xs w-full">
       <div
         className={cn(
           "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-          "bg-[url(https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80)] bg-cover",
-          // Preload hover image by setting it in a pseudo-element
-          "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
-          "transition-all duration-500"
+          "transition-all duration-500",
+          "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50"
         )}
+        style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover' }}
       >
         <div className="text relative z-50">
           <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
-            Background Overlays
+            {Title}
           </h1>
           <p className="font-normal text-base text-gray-50 relative my-4">
-            This card is for some special elements, like displaying background
-            gifs on hover only.
+            {Description}
           </p>
         </div>
       </div>
--- app/views/places/place.tsx
+++ app/views/places/place.tsx
@@ -8,24 +8,34 @@ export default function Place() {
                     <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">Spot Populer</h2>
 
                     <p className="mt-4 text-gray-500 sm:text-xl">
-                    Jelajahi keindahan alam kami yang menakjubkan, dari air terjun, yang akan mengisi album kenangan Anda.
+                        Jelajahi keindahan alam kami yang menakjubkan, dari air terjun, yang akan mengisi album kenangan Anda.
                     </p>
                 </div>
 
                 <div className="mt-8 sm:mt-12">
                     <dl className="grid grid-cols-1 gap-4 sm:grid-cols-3 sm:divide-x sm:divide-gray-100">
                         <div className="flex flex-col px-4 py-8 text-center">
-                            <Card 
+                            <Card
+                                Title="Judul Contoh"
+                                Description="Deskripsi contoh untuk kartu ini."
+                                Background="https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/indizone/2022/12/08/XxsdrW0/4-mitos-air-terjun-benang-kelambu-konon-tempat-pemandian-ratu-penguasa-gunung-rinjani86.jpg"
                             />
                         </div>
 
                         <div className="flex flex-col px-4 py-8 text-center">
-                            <Card 
+                            <Card
+                                Title="Judul Contoh"
+                                Description="Deskripsi contoh untuk kartu ini."
+                                Background="
+                                https://media-cdn.tripadvisor.com/media/photo-f/1a/74/c5/2f/photo4jpg.jpg"
                             />
                         </div>
 
                         <div className="flex flex-col px-4 py-8 text-center">
-                            <Card 
+                            <Card
+                                Title="Judul Contoh"
+                                Description="Deskripsi contoh untuk kartu ini."
+                                Background="https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/indizone/2022/12/08/XxsdrW0/4-mitos-air-terjun-benang-kelambu-konon-tempat-pemandian-ratu-penguasa-gunung-rinjani86.jpg"
                             />
                         </div>
                     </dl>
