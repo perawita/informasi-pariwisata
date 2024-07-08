fd95eb3df2fc55782e809150e4eea44e520c4982
perawitayasa <perawitayasa@gmail.com>
2024-07-08 16:54:30 +0000

    change button

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -12,9 +12,9 @@ export default function Footer() {
                             Join over 4,000+ startups already growing with Meraki UI.
                         </p>
                         <div className="flex flex-col mt-6 sm:flex-row sm:items-center sm:justify-center">
-                            <button className="w-full px-5 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:mx-2 sm:order-2 sm:w-auto hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                            <a href="/pesan-tiket" className="w-full px-5 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:mx-2 sm:order-2 sm:w-auto hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
                                 Get started
-                            </button>
+                            </a>
                         </div>
                     </div>
                     <div className="grid grid-cols-2 gap-6 mt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
@@ -24,40 +24,10 @@ export default function Footer() {
                             </h3>
                             <div className="flex flex-col items-start mt-4 space-y-4">
                                 <a
-                                    href="#"
+                                    href="/pesan-tiket"
                                     className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
                                 >
-                                    Overview
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Features
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Solutions
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Tutorials
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Pricing
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Releases
+                                    Tiket
                                 </a>
                             </div>
                         </div>
@@ -67,212 +37,22 @@ export default function Footer() {
                             </h3>
                             <div className="flex flex-col items-start mt-4 space-y-4">
                                 <a
-                                    href="#"
+                                    href="#lokasi"
                                     className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
                                 >
-                                    About us
+                                    Lokasi
                                 </a>
                                 <a
-                                    href="#"
+                                    href="#story"
                                     className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
                                 >
-                                    Careers
+                                    Sejarah
                                 </a>
                                 <a
-                                    href="#"
+                                    href="#most-populer"
                                     className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
                                 >
-                                    Press
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    News
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Media kit
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Contact
-                                </a>
-                            </div>
-                        </div>
-                        <div>
-                            <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                                Resources
-                            </h3>
-                            <div className="flex flex-col items-start mt-4 space-y-4">
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Blog
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Newsletter
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Events
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Help center
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Tutorials
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Supports
-                                </a>
-                            </div>
-                        </div>
-                        <div>
-                            <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                                Use cases
-                            </h3>
-                            <div className="flex flex-col items-start mt-4 space-y-4">
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Startups
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Enterprise
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Government
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Saas
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Marketplaces
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Ecommerce
-                                </a>
-                            </div>
-                        </div>
-                        <div>
-                            <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                                Social
-                            </h3>
-                            <div className="flex flex-col items-start mt-4 space-y-4">
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Twitter
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    LinkedIn
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Github
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Facebook
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    AngelList
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Dribble
-                                </a>
-                            </div>
-                        </div>
-                        <div>
-                            <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                                Legal
-                            </h3>
-                            <div className="flex flex-col items-start mt-4 space-y-4">
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Terms
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Privacy
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Cookies
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Licenses
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Settings
-                                </a>
-                                <a
-                                    href="#"
-                                    className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600"
-                                >
-                                    Contact
+                                    Wisata
                                 </a>
                             </div>
                         </div>
