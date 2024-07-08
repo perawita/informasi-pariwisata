7411a4d0a97be8ca69038bd6db6159dda3469836
perawitayasa <perawitayasa@gmail.com>
2024-07-08 16:04:09 +0000

    f

--- app/pesan-tiket/views/home.tsx
+++ app/pesan-tiket/views/home.tsx
@@ -24,7 +24,18 @@ export function Home() {
         <>
             <Header data={data} />
             <ImagesSlider className="h-[40rem]" images={images}>
-                <div
+                <motion.div
+                    initial={{
+                        opacity: 0,
+                        y: -80,
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: 0,
+                    }}
+                    transition={{
+                        duration: 0.6,
+                    }}
                     className="z-50 flex flex-col justify-center items-center"
                 >
                     <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
@@ -34,7 +45,7 @@ export function Home() {
                         <span>Dapatkan tiket anda →</span>
                         <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
                     </a>
-                </div>
+                </motion.div>
             </ImagesSlider>
         </>
     );
